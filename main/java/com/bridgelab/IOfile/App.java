61fb28c4d4dbbac49710589fa2388a47ea2207c9
rashg05 <gajra.rashmi0504@gmail.com>
2022-03-11 00:02:50 +0530

    Added: Use case 1 for creating employee payroll service read write operation

---
+++ main/java/com/bridgelab/IOfile/App.java
@@ -0,0 +1,9 @@
+package com.bridgelab.IOfile;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome To Employee Payroll service" );
+    }
+}
---
+++ main/java/com/bridgelab/IOfile/EmployeePayrollService.java
@@ -0,0 +1,31 @@
+package com.bridgelab.IOfile;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+	
+	ArrayList<EmployeePayrollData> employeePayrolldata = new ArrayList<>();
+	
+	public static void main(String[] args) {
+		EmployeePayrollService service = new EmployeePayrollService();
+		Scanner sc = new Scanner(System.in);
+		service.readEmployeePayrollData(sc);
+		service.writeEmployeePayrollData();
+	}
+	
+	public void readEmployeePayrollData(Scanner sc) {
+		System.out.println("Enter Employee ID: ");
+		int id = sc.nextInt();
+		System.out.println("Enter Employee Name: ");
+		String name = sc.next();
+		System.out.println("Enter Employee Salary: ");
+		double salary = sc.nextDouble();
+		employeePayrolldata.add(new EmployeePayrollData(id, name, salary));
+	}
+	
+	public void writeEmployeePayrollData() {
+		System.out.println("\nWriting Employee Payroll Roaster to console\n" + employeePayrolldata);
+	}
+
+}

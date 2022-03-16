0a41a0a552c672552b025496af2ad1e6b1fc6f27
rashg05 <gajra.rashmi0504@gmail.com>
2022-03-16 21:16:21 +0530

    Added: Use case 3 for watch service is added

--- main/java/com/bridgelab/IOfile/EmployeePayrollService.java
+++ main/java/com/bridgelab/IOfile/EmployeePayrollService.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+
+	public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+	
 	
 	ArrayList<EmployeePayrollData> employeePayrolldata = new ArrayList<>();
-	
+
 	public static void main(String[] args) {
 		EmployeePayrollService service = new EmployeePayrollService();
 		Scanner sc = new Scanner(System.in);
 		service.readEmployeePayrollData(sc);
 		service.writeEmployeePayrollData();
 	}
-	
+
 	public void readEmployeePayrollData(Scanner sc) {
 		System.out.println("Enter Employee ID: ");
 		int id = sc.nextInt();
@@ -23,7 +26,7 @@ public class EmployeePayrollService {
 		double salary = sc.nextDouble();
 		employeePayrolldata.add(new EmployeePayrollData(id, name, salary));
 	}
-	
+
 	public void writeEmployeePayrollData() {
 		System.out.println("\nWriting Employee Payroll Roaster to console\n" + employeePayrolldata);
 	}
--- test/java/com/bridgelab/IOfile/NIOFileAPITest.java
+++ test/java/com/bridgelab/IOfile/NIOFileAPITest.java
@@ -47,4 +47,13 @@ public class NIOFileAPITest {
 				path.toString().startsWith("temp"))
 		.forEach(System.out::println);
 	}
+	
+	@Test
+	public void givenADirectoryWhenWatchedListsAllTheActivities() throws IOException {
+		 
+		Path dir = Paths.get( HOME+ "/"+PLAY_WITH_NIO);
+		Files.list (dir).filter(Files::isRegularFile).forEach (System.out::println); 
+		new JavaWatchServiceExample(dir).processEvents();
+
+	}
 }
